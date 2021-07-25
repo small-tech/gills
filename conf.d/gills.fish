9cabf03b301d09301ff25c9553d0f3b0acdbee92
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-07-25 15:49:06 +0100

    Add pushd to ignored commands

--- conf.d/gills.fish
+++ conf.d/gills.fish
@@ -2,7 +2,7 @@
 # leave an empty space before the command to better
 # delinate the command output from the command itself.
 function add_empty_line --on-event fish_preexec
-    if not string match -r -q 'cd.*|popd.*' $argv
+    if not string match -r -q 'cd.*|popd.*|pushd.*' $argv
         printf '\n'
     end
 end
