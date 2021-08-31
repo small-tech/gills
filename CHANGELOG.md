40ecefea6ef123e66e595bf5548a53d51368ba48
Alper Kanat <me@alperkan.at>
2021-08-31 23:11:46 +0300

    New line around the prompt instead of just before it

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.1] - 2021-08-31
+
+- Print new line before & after the output of each prompt
+- Minor doc corrections
+
 ## [1.0.0] - 2021-07-25
 
 Initial release.
--- conf.d/gills.fish
+++ conf.d/gills.fish
@@ -1,7 +1,7 @@
 # Unless its a command known to not produce output,
-# leave an empty space before the command to better
+# leave an empty space around the command to better
 # delinate the command output from the command itself.
-function add_empty_line --on-event fish_preexec
+function add_empty_line --on-event fish_preexec --on-event fish_postexec
     if not string match -r -q 'cd.*|popd.*|pushd.*' $argv
         printf '\n'
     end
