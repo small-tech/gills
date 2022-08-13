ef7eef38aed503ce7db36fbeac91806094b4662a
Aral Balkan <mail@ar.al>
2022-08-13 10:08:12 +0100

    Fix extra line on new shells and following clear command

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.1] - 2022-08-13
+
+### Fixed
+
+  - Does not leave extra empty line when shell initially launches or when clear command is used.
+
 ## [1.0.0] - 2021-07-25
 
 Initial release.
--- conf.d/gills.fish
+++ conf.d/gills.fish
@@ -1,7 +1,14 @@
+# Unless we’re clearing the screen, add a line before the prompt.
+function add_empty_line_before_prompt --on-event fish_postexec
+    if not string match -r -q 'clear.*' $argv
+        printf '\n'
+    end
+end
+
 # Unless its a command known to not produce output,
 # leave an empty space before the command to better
 # delinate the command output from the command itself.
-function add_empty_line --on-event fish_preexec
+function add_empty_line_before_command --on-event fish_preexec
     if not string match -r -q 'cd.*|popd.*|pushd.*' $argv
         printf '\n'
     end
